8f48d8d97634627a400e94b892c9e8f3a03f4a2c
Alexander Dollard <alexdollard141@gmail.com>
2025-03-29 18:37:36 -0500

    updated hero image and color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        customFont: ["Bebas Neue", "sans-serif"],
+      },
+      fontWeight: {
+        light: "300",
+        normal: "400",
+      },
+      backgroundImage: {
+        "hero-image": "url('/images/barbell.jpg')",
+        "hero-image-2": "url('/images/hero2.jpg')",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
